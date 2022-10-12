64999936d6f1b3d9e24d2bfd5e9e903d78a8097c
Renat Prokopiev <prokop4891@gmail.com>
2022-10-12 22:38:03 +0300

    2nd task

---
+++ 002_InQ#ReturnXY/Program.cs
@@ -0,0 +1,27 @@
+﻿int PrintAndGetValue(string message)
+{
+    Console.WriteLine(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int input = PrintAndGetValue("Введите № четверти");
+
+switch(input)
+{
+    case 1:
+        Console.WriteLine("X > 0; Y > 0");
+        break;
+    case 2:
+        Console.WriteLine("X < 0; Y > 0");
+        break;
+    case 3:
+        Console.WriteLine("X < 0; Y < 0");
+        break;
+    case 4:
+        Console.WriteLine("X > 0; Y < 0");
+        break;
+    default:
+        Console.WriteLine("Ошибка ввода");
+        break;
+}
