920c87992ef41f9649525d64dd304b0ea3aa72b3
Renat Prokopiev <prokop4891@gmail.com>
2022-10-12 23:33:19 +0300

    4th task

---
+++ 004_Pov/Program.cs
@@ -0,0 +1,31 @@
+﻿int PrintAndGetValue(string message)
+{
+    Console.WriteLine(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int N = PrintAndGetValue("Введите целочисленное число");
+
+if (N > 0)
+{
+    for(int i = 1; i<=N; i++)
+    {
+        double result = Math.Pow(i,2);
+        Console.Write(result +" ");
+    }
+}
+else if (N < 0)
+{
+    for(int i = N; i<0; i++)
+    {
+        double result = Math.Pow(i,2);
+        Console.Write(result +" ");
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка");
+}
+
+
