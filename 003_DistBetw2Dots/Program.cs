b1423a2c5e4e24b45612123d69843d511dea5b46
Renat Prokopiev <prokop4891@gmail.com>
2022-10-12 23:11:48 +0300

    3rd task

---
+++ 003_DistBetw2Dots/Program.cs
@@ -0,0 +1,17 @@
+﻿int PrintAndGetValue(string message)
+{
+    Console.WriteLine(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int Xa = PrintAndGetValue("Введите координтау X для точки А");
+int Ya = PrintAndGetValue("Введите координтау Y для точки А");
+int Xb = PrintAndGetValue("Введите координтау X для точки B");
+int Yb = PrintAndGetValue("Введите координтау Y для точки B");
+
+double xPow = Math.Pow((Xa-Xb), 2);
+double yPow = Math.Pow((Ya-Yb), 2);
+double AB = Math.Sqrt(xPow+yPow);
+
+Console.WriteLine(AB);
