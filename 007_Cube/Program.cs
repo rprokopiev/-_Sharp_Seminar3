f784c5fe474e0d5df9da7b88534009e495ff40f7
Renat Prokopiev <prokop4891@gmail.com>
2022-10-14 12:14:20 +0300

    3rd HW

---
+++ 007_Cube/Program.cs
@@ -0,0 +1,18 @@
+﻿int PrintReadIntReturn(string message)
+{
+    Console.WriteLine(message);
+    int N = int.Parse(Console.ReadLine());
+    return N;
+}
+
+int Number = PrintReadIntReturn("Введите число");
+int i = 1;
+while(i < Number)
+{
+    double N = Math.Pow(i, 3);
+    Console.Write(N);
+    Console.Write(", ");
+    i++;
+}
+double Last = Math.Pow(Number, 3);
+Console.Write(Last);
