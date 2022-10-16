0c404be2a857b7e539fc56520b10e3f240d991c7
Renat Prokopiev <prokop4891@gmail.com>
2022-10-16 22:24:30 +0300

    1st Seminar task

---
+++ 008_Sum1toN/Program.cs
@@ -0,0 +1,38 @@
+﻿
+bool IsInputValid(int a)
+{
+    if(a<1)
+    {
+        return false;
+    }
+    return true;
+}
+
+Console.WriteLine("Enter A");
+int A = int.Parse(Console.ReadLine());
+bool isValid = IsInputValid(A);
+if (isValid)
+{
+    int sum = CalculateSum(A);
+    Console.WriteLine("Sum = " + sum);
+
+}
+else
+{
+    Console.WriteLine("error");
+}
+
+int CalculateSum(int n)
+{
+    int sum = 0;
+    for (int i = 1; i<=n; i++)
+    {
+        sum+=i;
+    }
+    return sum;
+}
+
+
+
+
+
