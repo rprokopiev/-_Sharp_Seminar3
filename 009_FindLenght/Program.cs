5ac5f00ca1f75233f8de533f4116b8f3bf3fbeee
Renat Prokopiev <prokop4891@gmail.com>
2022-10-16 23:30:07 +0300

    8 9 SemTasks

---
+++ 009_FindLenght/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter number");
+int N =int.Parse(Console.ReadLine());
+
+int m = 0;
+
+while (N=>0)
+{    
+    N = N / 10;
+    m++;
+}
+Console.WriteLine(m);
---
+++ 010_Mult1_N/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int Num = int.Parse(Console.ReadLine());
+int m = 1;
+int i = 1;
+while (i <= Num)
+{
+    m = m*i;
+    i++;
+}
+Console.WriteLine(m);
