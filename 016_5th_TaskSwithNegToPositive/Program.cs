0372202cb5db1b181c4793583a23148ebc8bec13
Renat Prokopiev <prokop4891@gmail.com>
2022-10-18 20:46:47 +0300

    test

---
+++ 016_5th_TaskSwithNegToPositive/Program.cs
@@ -0,0 +1,27 @@
+﻿void SwitchArrayPositNegative(int [] array)
+{
+    int i = 0;
+    while (i < array.Length)
+    {
+        if (array[i] < 0)
+        {
+            array[i] = array [i] + (2 * array [i]);
+            i++;
+        }
+        else
+        {
+            array[i] = array[i] - (2 * array[i]);
+            i++;
+        }
+    }
+}
+
+int [] test = {1,2,3,4,5};
+SwitchArrayPositNegative(test);
+// int i = 0;
+// while(i < test.Length)
+// {
+//     Console.Write(test[i] + " / ");
+//     i++;
+// }
+
