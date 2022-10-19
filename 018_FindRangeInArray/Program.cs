f3587e9f5c377ecce4311e19c8b2e8b8909cf203
Renat Prokopiev <prokop4891@gmail.com>
2022-10-19 08:17:12 +0300

    5ht Sem 3rd task

---
+++ 018_FindRangeInArray/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillRandArrayUserSet (int [] Array)
+{ 
+    int index = 0;
+    Console.WriteLine("Введите максимально значение массива");
+    int max = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите минимальное значение массива");
+    int min = int.Parse(Console.ReadLine());
+    while (index < Array.Length)
+    {
+        Array[index] = new Random().Next(min,max);
+        index++;
+    }
+}
+
+int [] array = new int [123];
+FillRandArrayUserSet(array);
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]>=10 && array[i]<=99)
+    {
+        count = count + 1;
+        i++;
+    }
+}
+Console.WriteLine(count);
