399d022d6615b7e9c228a3f737fa289d175fd00a
Renat Prokopiev <prokop4891@gmail.com>
2022-10-20 21:24:23 +0300

    1st 2nd Sem tasks

---
+++ 023_6th_array_revert/Program.cs
@@ -0,0 +1,17 @@
+﻿void FillRandArrayUserSet (double [] Array)
+{ 
+    Console.WriteLine("Введите длинну массива");
+    int Lght = int.Parse(Console.ReadLine());
+    int index = 0;
+    Console.WriteLine("Введите максимально значение массива");
+    int max = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите минимальное значение массива");
+    int min = int.Parse(Console.ReadLine());
+    while (index < Array.Length)
+    {
+        Array[index] = new Random().Next(min, max);
+        index++;
+    }
+}
+
+
---
+++ 024_6th_TringleSidesCheck/Program.cs
@@ -0,0 +1,19 @@
+﻿void IsTrianglePossible (string message1, string message2, string message3)
+{
+    Console.WriteLine(message1);
+    int A = int.Parse(Console.ReadLine());
+    Console.WriteLine(message2);
+    int B = int.Parse(Console.ReadLine());
+    Console.WriteLine(message3);
+    int C = int.Parse(Console.ReadLine());
+    if (A < B + C && B < A + C && C < A + B)
+    {
+        Console.WriteLine("Треугольник со сторонами " + A + ", " + B + " и " + C + " возможен.");
+    }
+    else
+    {
+        Console.WriteLine("Не может быть построен треугольник со сторонами " + A + ", " + B + " и " + C +".");
+    }       
+}
+
+IsTrianglePossible("Введите длинну 1й стороны", "2й", "и 3й");
