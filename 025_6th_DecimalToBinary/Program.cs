874b808146207f47ddd4844f22c43b014c32282b
Renat Prokopiev <prokop4891@gmail.com>
2022-10-21 00:04:30 +0300

    2nd save

---
+++ 025_6th_DecimalToBinary/Program.cs
@@ -0,0 +1,51 @@
+﻿int Prompt (string message)
+{
+    Console.WriteLine(message);
+    int A = int.Parse(Console.ReadLine());
+    return A;
+}
+
+int DigitQtyInBinar (int number)
+{
+    int Lght = 0;
+    int var = number;
+    while (var>0)
+    {
+        var = var / 2;
+        Lght++;
+    }
+    return Lght;
+}
+
+void GetArrayOfBinaryDigits (int [] binArr, int number)
+{
+    int var2 = number;
+    for (int i = binArr.Length - 1; i > -1; i--)
+    {
+        binArr[i] = var2 - ((var2 / 2)*2);
+        var2 = var2 / 2;
+    }
+}
+
+void WriteArray (int [] array)
+{
+    int i = 0;
+    while (i < array.Length)
+    {
+        Console.Write(array[i]);
+        i++;
+    }
+}
+
+int N = Prompt("Введите положительное число");
+
+int Lght = DigitQtyInBinar(N);
+
+int [] list = new int [Lght];
+GetArrayOfBinaryDigits (list, N);
+WriteArray(list);
+
+string correct = Convert.ToString(-25,2);
+Console.WriteLine();
+Console.WriteLine(correct);
+
---
+++ 025v1_DecimalToBilinary/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("enter");
+int number = int.Parse(Console.ReadLine());
+
+int k;
+string result = "";
+
+while (number > 0);
+{
+    k = number % 2;
+    number = number / 2;
+    result = k + result;
+}
+
+// Console.WriteLine(result);
+// Console.WriteLine(Convert.ToString(number,2));
+Console.WriteLine(Conv(number));
+
+string Conv (int number)
+{
+    if (number == 0) return "";
+    return k + Conv (number / 2);
+}
