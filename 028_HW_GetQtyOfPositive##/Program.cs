0daf53c21dcd59be269a32199ce67645990d2540
Renat Prokopiev <prokop4891@gmail.com>
2022-10-22 23:14:06 +0300

    HW Task 41

---
+++ 028_HW_GetQtyOfPositive##/Program.cs
@@ -0,0 +1,69 @@
+﻿int GetNumber (string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void PrintArray (int [] array)
+{
+    Console.Write("[");
+    int i = 0;
+    while (i < array.Length-1)
+    {
+        Console.Write(array[i] + ", ");
+        i++;
+    }
+    Console.WriteLine(array[array.Length-1] + "]");
+}
+
+void FillArrayFromTerminal(int [] array)
+{
+    int i = 0;
+    int count = 1;
+    array[i] = GetNumber("Введите первое число");
+    i++;
+    count++;
+    while (i<array.Length-1)
+    {
+        array[i] = GetNumber("Введите следующее число, номер " + count);
+        i++;
+        count++;
+    }
+    array[array.Length-1] = GetNumber("Введите последнее число");
+}
+
+int GetQtyOfPositiveNumbs (int [] array)
+{
+    int i = 0;
+    int count = 0;
+    while (i < array.Length)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+            i++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return count;
+}
+
+int Lght = GetNumber ("Введите кол-во чисел, которое хотите ввести");
+
+if (Lght > 0)
+{
+    int [] Array = new int [Lght];
+    FillArrayFromTerminal(Array);
+    Console.Write("Введено ");
+    PrintArray(Array);
+    int result = GetQtyOfPositiveNumbs(Array);
+    Console.WriteLine("Кол-во числел больше 0 = " + result);
+}
+else
+{
+    Console.WriteLine("Ошибка ввода");
+}
---
+++ _UsefulMethods/Program.cs
@@ -0,0 +1,44 @@
+﻿int GetNumber (string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void FillArrayWithRandomNumbers(int [] array, int max, int min)
+{
+    int i = 0;
+    while (i<array.Length)
+    {
+        array[i] = new Random().Next(min, max);
+        i++;
+    }
+}
+
+void PrintArray (int [] array)
+{
+    Console.Write("[");
+    int i = 0;
+    while (i < array.Length-1)
+    {
+        Console.Write(array[i] + ", ");
+        i++;
+    }
+    Console.WriteLine(array[array.Length-1] + "]");
+}
+
+void FillIntArrayFromTerminal(int [] array)
+{
+    int i = 0;
+    int count = 1;
+    array[i] = GetNumber("Введите первое число");
+    i++;
+    count++;
+    while (i<array.Length-1)
+    {
+        array[i] = GetNumber("Введите следующее число, номер " + count);
+        i++;
+        count++;
+    }
+    array[array.Length-1] = GetNumber("Введите последнее число");
+}
