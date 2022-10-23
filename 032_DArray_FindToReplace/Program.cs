f646d0c33e4c9e2dce45a297143c1f8e3885017b
Renat Prokopiev <prokop4891@gmail.com>
2022-10-23 23:51:13 +0300

    the last tasks of 7th Sem

---
+++ 032_DArray_FindToReplace/Program.cs
@@ -0,0 +1,58 @@
+﻿int GetNumber (string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int[,] CreateArray(int n, int m, int min, int max)
+{
+    int [,] array = new int[n, m];
+    for (int rows = 0; rows < n; rows++)
+    {
+        for (int columns = 0; columns < m; columns++)
+        {
+            array[rows,columns] = new Random().Next(min,max+1);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < array.GetLength(1); columns++)
+        {
+            Console.Write($"{array[rows,columns]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] CreateDublicateWithPow(int[,] array)
+{
+    int [,] array1 = new int [array.GetLength(0),array.GetLength(1)];
+
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for(int columns = 0; columns < array.GetLength(1); columns++)
+        {
+            if (rows % 2 ==1 && columns % 2 ==1)
+            {
+                array1[rows,columns] = array[rows,columns]*array[rows,columns];
+            }
+            else
+            {
+                array1[rows,columns] = array[rows,columns];
+            }          
+        }
+    }
+    return array1;
+}
+
+int n = GetNumber("Enter rows qty");
+int m = GetNumber("Enter columns qty");
+int [,] Array = CreateArray(n, m, 1, 10);
+PrintArray(Array);
+int [,] resultArray = CreateDublicateWithPow(Array);
+Console.WriteLine();
+PrintArray(resultArray);
+PrintArray(Array);
---
+++ 033_DArray_SumOfDiagonal/Program.cs
@@ -0,0 +1,50 @@
+﻿int GetNumber (string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int[,] CreateArray(int n, int m, int min, int max)
+{
+    int [,] array = new int[n, m];
+    for (int rows = 0; rows < n; rows++)
+    {
+        for (int columns = 0; columns < m; columns++)
+        {
+            array[rows,columns] = new Random().Next(min,max+1);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < array.GetLength(1); columns++)
+        {
+            Console.Write($"{array[rows,columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int GetDArrayDiagonalSum(int[,] array)
+{
+    int sum = 0;
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for(int columns = 0; columns < array.GetLength(1); columns++)
+        {
+            if (rows == columns) sum = sum + array[rows,columns];
+        }
+    }
+    return sum;
+}
+
+int n = GetNumber("Enter rows qty");
+int m = GetNumber("Enter columns qty");
+int [,] Array = CreateArray(n, m, 1, 10);
+PrintArray(Array);
+int DiagSum = GetDArrayDiagonalSum(Array);
+Console.WriteLine();
+Console.WriteLine(DiagSum);
+
