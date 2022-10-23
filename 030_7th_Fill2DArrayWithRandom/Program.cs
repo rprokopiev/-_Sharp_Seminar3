982328ea8c02bb9c7a448e8db501660c8ca7561e
Renat Prokopiev <prokop4891@gmail.com>
2022-10-23 22:16:28 +0300

    1st and 2nd taskc of 7th Sem

---
+++ 030_7th_Fill2DArrayWithRandom/Program.cs
@@ -0,0 +1,46 @@
+﻿int GetNumber (string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+bool IsInputLenghtValid(int number)
+{
+    if (number <= 0)
+    {
+        return false;
+    }
+    return true;
+}
+
+int[,] CreateArray(int n, int m, int min, int max)
+{
+    int [,] array = new int[n, m];
+    for (int rows = 0; rows < n; rows++)
+    {
+        for (int columns = 0; columns < m; columns++)
+        {
+            array[rows,columns] = new Random().Next(min,max+1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < array.GetLength(1); columns++)
+        {
+            Console.Write($"{array[rows,columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int n = GetNumber("ВВедите кол-во строк");
+int m = GetNumber("Введите кол-во столбцов");
+int [,] Array = CreateArray(n,m,-10,10);
+PrintArray(Array);
+
---
+++ 031_7th_DArray/Program.cs
@@ -0,0 +1,36 @@
+﻿int GetNumber (string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[,] CreateArray(int n, int m)
+{
+    int [,] array = new int[n, m];
+    for (int rows = 0; rows < n; rows++)
+    {
+        for (int columns = 0; columns < m; columns++)
+        {
+            array[rows,columns] = rows + columns;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < array.GetLength(1); columns++)
+        {
+            Console.Write($"{array[rows,columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int n = GetNumber("Enter rows quantity");
+int m = GetNumber("Enter columns quantity");
+int [,] Array = CreateArray(n,m);
+PrintArray(Array);
