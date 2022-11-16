98f161a8bc810b3d524e1daf9d5d77732ce6de18
Renat Prokopiev <prokop4891@gmail.com>
2022-11-16 20:38:34 +0300

    9th Sem 1st HW

---
+++ 049_HW_GetNumbers_Nto1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+int N = 9;
+
+string GetNumber(int number)
+{
+    Console.Write(number + ", ");
+    if (number == 2) 
+    {
+        Console.Write(number-1); 
+        return string.Empty;
+    }
+    else return GetNumber(number-1);
+}
+
+GetNumber(N);
